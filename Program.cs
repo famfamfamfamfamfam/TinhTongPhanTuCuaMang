928cf74abd9431683b57d52d6014fc4749bcb0e2
BinhTran <fam.out16@gmail.com>
2024-08-13 06:54:41 +0700

    Add cac files cua bai thuc hanh tinh tong phan tu trong mang

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MANGMOTCHIEU
+{
+    class TinhTongPhanTuMang
+    {
+        static void Main(string[] args)
+        {
+            int[] mang = new int[5];
+            mang[0] = 2;
+            mang[1] = 5;
+            mang[2] = 9;
+            mang[3] = 6;
+            mang[4] = 7;
+            
+            int tong = 0;
+            for(int i= 0; i<5; i++)
+            {
+                tong += mang[i];
+            }
+            Console.WriteLine(tong);
+        }
+    }
+}
